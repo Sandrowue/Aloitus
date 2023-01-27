a32bd3317f062540bccd2d0661b01884163a3d41
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2023-01-27 12:08:29 +0200

    kokeiltu toinen  versio

--- kysy_tiedot_fitness.py
+++ kysy_tiedot_fitness.py
@@ -2,14 +2,38 @@
 
 # Kirjastot ja Moduulit
 
+# Tuodaan fitness.py:n sisältämät toiminnot tähän ohjelmaan.
 import fitness
 
-pituus_teksti = input('Pituus (cm): ')
-paino_teksti = input('Paino (kg): ')
+# Kysytään tiedot ja tulostetaan painoindeksi kunnes halutaan lopettaa
 
-pituus = float(pituus_teksti)
-paino = float(paino_teksti)
+'''pituus_teksti = '0'
+while pituus_teksti != '':
 
-bmi = fitness.laske_bmi(paino, pituus)
+    pituus_teksti = input('Pituus (cm), (stop = tyhjä): ')
+    if pituus_teksti == '':
+        break
+    paino_teksti = input('Paino (kg): ')
 
-print('Painoindeksi on', bmi)
+    pituus = float(pituus_teksti)
+    paino = float(paino_teksti)
+    # lasketaan painoindeksin fitness-modulin laske_bmi funktiolla
+    bmi = fitness.laske_bmi(paino, pituus)
+
+    print('Painoindeksi on', bmi)'''
+
+
+
+while True:
+    
+    pituus_teksti = input('Pituus (cm), (stop = tyhjä): ')
+    if pituus_teksti == '':
+        break
+    paino_teksti = input('Paino (kg): ')
+
+    pituus = float(pituus_teksti)
+    paino = float(paino_teksti)
+    # lasketaan painoindeksin fitness-modulin laske_bmi funktiolla
+    bmi = fitness.laske_bmi(paino, pituus)
+
+    print('Painoindeksi on', bmi)
