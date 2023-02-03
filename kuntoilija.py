c3896566aba2ed07e91e0f64517166abf3148a3a
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2023-02-03 10:18:45 +0200

    metodit lisätty

--- kuntoilija.py
+++ kuntoilija.py
@@ -8,6 +8,7 @@ import fitness
 class Kuntoilija:
     """Luokka kuntoilijan tietoja varten
     """
+    # Olionmuodostin eli konstruktori
     def __init__(self, nimi, pituus, paino, ika, sukupuoli):
         # Määritellään tulevan olion ominaisuudet (property), luokan kentät (field)
         self.nimi = nimi
@@ -16,7 +17,17 @@ class Kuntoilija:
         self.ika = ika
         self.sukupuoli = sukupuoli
 
+# Metodi painoindeksin laskemiseen
+    def painoindeksi(self):
+        self.bmi = fitness.laske_bmi(self.paino, self.pituus)
+        return self.bmi
+
+ # Metodi aikuisen rasvaprosentin laskemiseen
+    def rasvaprosentti(self):
+        rasvaprosentti = fitness.aikuisen_rasvaprosentti(self.bmi, self.ika, self.sukupuoli)
+
 if __name__ == "__main__":
     
     kuntoilija = Kuntoilija('Kalle Kuntoilija', 171, 56, 22, 'mies')
     print(kuntoilija.nimi, 'painaa', kuntoilija.paino, 'kg')
+    print('painoindeksi on', kuntoilija.painoindeksi())
