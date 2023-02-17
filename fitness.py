57b446e9640ccd110a7910f6a13cf755869daaf2
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2023-02-17 11:21:42 +0200

    USAn rasvaprosentti

--- fitness.py
+++ fitness.py
@@ -1,5 +1,8 @@
 # SOVELLUS PAINOINDEKSIN JA KEHON RASVAPROSENTIN LASKEMISEEN    
 
+# Kirjastot ja moduulit
+import math
+
 # Määritellään funktio painoindeksin laskentaan
 def laske_bmi(paino, pituus):
     """Laskee painoindeksin (BMI)
@@ -51,6 +54,29 @@ def lapsen_rasvaprosentti(bmi, ika, sukupuoli):
     rasvaprosentti = round(bmi * 1.51 - 0.7 * ika - 3.6 * sukupuoli + 1.4, 1) 
     return rasvaprosentti
 
+
+def usarasvaprosentti_mies(pituus, vyotaron_ymparys, kaulan_ymparys):
+    """Laskee miehen rasvaprosentin USA:n armejan kaavalla
+
+    Args:
+        pituus (float): pituus (cm)
+        vyötärön_ymp (float): vyötärön ympärysmitta (cm)
+        kaulan_ymp (float): kaulan ympärysmitta (cm)
+
+
+    Returns:
+        float: rasvaprosentti
+    """
+    # Muutetaan mitat tuumiksi
+    tuuma_pituus = pituus / 2.54
+    tuuma_vyotaron_ymparys = vyotaron_ymparys / 2.54
+    tuuma_kaulan_ymparys = kaulan_ymparys / 2.54
+    # Lasketaan rasvaprosentti
+    usarprosentti = round(86.01 * math.log10(tuuma_vyotaron_ymparys - tuuma_kaulan_ymparys) - 70.041 * math.log10(tuuma_pituus) + 36.76, 2178)
+    return usarprosentti 
+
+
+
 if __name__ == '__main__':
     # Suoritetaan seuraavat rivit vain, jos tämä tiedosto on pääohjelma
     # Mahdollistaa funktioiden jakaamisen toisiin ohjelmiin
@@ -60,12 +86,16 @@ if __name__ == '__main__':
     paino_teksti = input('Kuinka paljon painat(kg)?')
     ika_teksti = input('Kuinka vanha olet?')
     sukupuoli_teksti = input('Sukupuoli: mies - vastaa: 1 / nainen - vastaa: 0:') 
+    vyotaron_ymparys_teksti = input('Mikä on vyötärön ympäryksesi? (cm):')
+    kaulan_ymparys_teksti = input('Mikä on kaulan ympärysmitta (cm):')
 
     pituus = float(pituus_teksti)
     paino = float(paino_teksti)
     ika = float(ika_teksti)
     sukupuoli = float(sukupuoli_teksti)
     oma_bmi = laske_bmi(paino, pituus)
+    vyotaron_ymparys = float(vyotaron_ymparys_teksti)
+    kaulan_ymparys = float(kaulan_ymparys_teksti)
 
     if ika >= 18:
         oma_rasvaprosentti = aikuisen_rasvaprosentti(oma_bmi, ika, sukupuoli)
@@ -73,3 +103,6 @@ if __name__ == '__main__':
         oma_rasvaprosentti = lapsen_rasvaprosentti(oma_bmi, ika, sukupuoli)
 
     print('Painoindeksisi on:', oma_bmi, 'ja kehon rasvaprosentti on:', oma_rasvaprosentti)
+
+    usa_rasvaprosentti = usarasvaprosentti_mies(pituus, vyotaron_ymparys, kaulan_ymparys)
+    print('USA:n armeijan kaavalla rasvaprosentti on', usa_rasvaprosentti)
